0552c980fbaf21f35046707d20626c878ef7e157
Jawad_Ali <96284750+AliJawad123@users.noreply.github.com>
2023-12-26 23:05:45 +0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+
+# Install Python 3 and pip
+sudo apt-get install -y python3 python3-pip
+
+# Install additional dependencies if needed
+# Example: sudo apt-get install -y <your-dependency>
+
+# Install required Python packages using pip
