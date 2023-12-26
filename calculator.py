759c50e30813757d055ced4997dfb6744c21765c
jawad <jawadali123@gmail.com>
2023-12-26 22:21:03 +0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+
+def on_click(value):
+    current = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, current + value)
+
+def clear_entry():
+    entry.delete(0, tk.END)
+
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(result))
+    except Exception as e:
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "Error")
+
+# Create the main window
+root = tk.Tk()
+root.title("Calculator")
+
+# Entry widget to display input and results
+entry = tk.Entry(root, width=20, borderwidth=5, font=('Arial', 14))
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+# Define buttons
+buttons = [
+    ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
+    ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
+    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+    ('0', 4, 0), ('C', 4, 1), ('=', 4, 2), ('+', 4, 3)
+]
+
+# Add buttons to the grid
+for (text, row, col) in buttons:
+    button = tk.Button(root, text=text, padx=20, pady=20, font=('Arial', 14),
+                       command=lambda t=text: on_click(t) if t != '=' else calculate() if t == '=' else clear_entry())
+    button.grid(row=row, column=col)
+
+# Start the main loop
+root.mainloop()
